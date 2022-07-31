87534de9b452c05291b555b705bbbf7d767a91f3
Yurii Kurleiev <ykurleiev@Yuriis-MacBook-Pro.local>
2022-07-31 14:54:47 +0300

    added Prisma and basic /vote endpoint

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,15 +1,34 @@
 import * as trpc from '@trpc/server';
 import { z } from 'zod';
 import { PokemonClient } from 'pokenode-ts';
+import { prisma } from '@/backend/utils/prisma';
 
-export const appRouter = trpc.router().query('get-pokemon-by-id', {
-  input: z.object({
-    id: z.number(),
-  }),
-  async resolve({ input }) {
-    const api = new PokemonClient();
-    return await api.getPokemonById(input.id);
-  },
-})
+export const appRouter = trpc.router()
+  .query('get-pokemon-by-id', {
+    input: z.object({
+      id: z.number(),
+    }),
+    async resolve({ input }) {
+      const api = new PokemonClient();
+      const { name, sprites } = await api.getPokemonById(input.id);
+      return { name, sprites };
+    },
+  })
+  .mutation('cast-vote', {
+    input: z.object({
+      voteFor: z.number(),
+      voteAgainst: z.number(),
+    }),
+    async resolve({ input }) {
+      const voteInDb = await prisma.vote.create({
+        data: {
+          ...input
+        }
+      });
+      return {
+        success: true,
+      };
+    }
+  });
 
 export type AppRouter = typeof appRouter;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import { useState } from 'react';
-import { getOptionsForVote } from '@/utils/getRandomPokemon';
-import { trpc } from '@/utils/trpc';
+import React, { useState } from 'react';
+import { getOptionsForVote } from '@/backend/utils/getRandomPokemon';
+import { trpc } from '@/backend/utils/trpc';
+import { inferQueryResponse } from '@/pages/api/trpc/[trpc]';
+
+const btn = 'inline-flex items-center px-2.5 py-1.5 border border-gray300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500';
 
 export default function Home() {
-  const [ids, updateIds] = useState(getOptionsForVote());
+  const [ids, updateIds] = useState(() => getOptionsForVote());
   const [first, second] = ids;
 
   const firstPokemon = trpc.useQuery(['get-pokemon-by-id', {id: first}]);
@@ -11,30 +14,51 @@ export default function Home() {
 
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
+  const voteForRoundest = (selected: number) => {
+    updateIds(getOptionsForVote());
+  };
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="p-2"/>
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-64 h-64 flex flex-col">
-          <img
-            src={firstPokemon.data?.sprites.front_default!}
-            alt="First Pokemon"
-            className="w-full"
-          />
-          <div className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>
-        </div>
-        <div className="p-8">Vs</div>
-        <div className="w-64 h-64 flex flex-col">
-          <img
-            src={secondPokemon.data?.sprites.front_default!}
-            alt="Second Pokemon"
-            className="w-full"
-          />
-          <div className="text-xl text-center capitalize mt-[-2rem]">{secondPokemon.data?.name}</div>
-        </div>
+        {
+          !firstPokemon.isLoading &&
+          firstPokemon.data &&
+          !secondPokemon.isLoading &&
+          secondPokemon.data && (
+            <>
+              <PokemonListing
+                pokemon={firstPokemon.data}
+                vote={() => voteForRoundest(first)}
+              />
+              <div className="p-8">Vs</div>
+              <PokemonListing
+                pokemon={secondPokemon.data}
+                vote={() => voteForRoundest(second)}
+              />
+            </>
+          )
+        }
         <div className="p-2" />
       </div>
+    </div>
+  )
+}
+
+type PokemonFromServer = inferQueryResponse<"get-pokemon-by-id">;
+
+const PokemonListing: React.FC<{ pokemon: PokemonFromServer, vote: () => void }> = ({ pokemon, vote }) => {
+  return (
+    <div className="flex flex-col items-center">
+      <img
+        src={pokemon.sprites.front_default!}
+        alt="First Pokemon"
+        className="w-64 h-64"
+      />
+      <div className="text-xl text-center capitalize mt-[-2rem]">{pokemon.name}</div>
+      <button onClick={() => vote()} className={btn}>Rounder</button>
     </div>
   )
 }
